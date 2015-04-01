aec732256eeb57dd1c98a0e1e671184059fb3c68
sirjoblack <sirjoblack@hotmail.com>
2015-04-02 00:40:30 +0200

    clean

--- project/sblk85/spi.d
+++
@@ -1,4 +0,0 @@
-spi.o: ../spi.cpp ../spi.h ../circbuff.h ../include/h_types.h \
- ../include/fnasm.h ../include/reg/timing.h ../include/reg/interrupt.h \
- ../include/reg/regusi.h ../include/reg/regdport.h ../include/bits.h \
- ../include/reg/regmacros.h
